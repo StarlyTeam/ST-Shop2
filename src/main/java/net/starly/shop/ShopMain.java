6ba59a5f7fe14bc5eab3297be7c4a10234b34632
yejunho10 <hyjcompany30@naver.com>
2023-04-02 13:59:37 +0900

    fix: Bugs

--- src/main/java/net/starly/shop/ShopMain.java
+++ src/main/java/net/starly/shop/ShopMain.java
@@ -75,7 +75,7 @@ public class ShopMain extends JavaPlugin {
         try {
             Class.forName(path);
             return true;
-        } catch (NoClassDefFoundError ignored) {
+        } catch (ClassNotFoundException ignored) {
         } catch (Exception ex) { ex.printStackTrace(); }
         return false;
     }
